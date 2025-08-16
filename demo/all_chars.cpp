30beea508dbe34ed8450395a98a829f2170a4760
Paul Bowen-Huggett <paulhuggett@mac.com>
2025-08-16 18:22:58 +0200

    Add boilerplate text

--- demo/all_chars.cpp
+++ demo/all_chars.cpp
@@ -1,4 +1,34 @@
-// Draw
+//===- demo/all_chars.cpp -------------------------------------------------===//
+//*        _ _        _                     *
+//*   __ _| | |   ___| |__   __ _ _ __ ___  *
+//*  / _` | | |  / __| '_ \ / _` | '__/ __| *
+//* | (_| | | | | (__| | | | (_| | |  \__ \ *
+//*  \__,_|_|_|  \___|_| |_|\__,_|_|  |___/ *
+//*                                         *
+//===----------------------------------------------------------------------===//
+// Copyright © 2025 Paul Bowen-Huggett
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// “Software”), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+//===----------------------------------------------------------------------===//
 #include "draw/bitmap.hpp"
 #include "draw/glyph_cache.hpp"
 #include "draw/sans16.hpp"
--- include/draw/plru_cache.hpp
+++ include/draw/plru_cache.hpp
@@ -1,3 +1,34 @@
+//===- include/draw/plru_cache.hpp ------------------------*- mode: C++ -*-===//
+//*        _                             _           *
+//*  _ __ | |_ __ _   _    ___ __ _  ___| |__   ___  *
+//* | '_ \| | '__| | | |  / __/ _` |/ __| '_ \ / _ \ *
+//* | |_) | | |  | |_| | | (_| (_| | (__| | | |  __/ *
+//* | .__/|_|_|   \__,_|  \___\__,_|\___|_| |_|\___| *
+//* |_|                                              *
+//===----------------------------------------------------------------------===//
+// Copyright © 2025 Paul Bowen-Huggett
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// “Software”), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+//===----------------------------------------------------------------------===//
 /// Implements a "Tree-PLRU" (Pseudo Least-recently Used) unordered associative container. It is
 /// intended as a small cache for objects which are relatively cheap to store and relatively expensive
 /// to create. The container's keys must be unsigned integral types.
