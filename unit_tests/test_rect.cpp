d8401a28c2cf3aee3b1606cd1c9de22995452517
Paul Bowen-Huggett <paulhuggett@mac.com>
2026-02-09 21:33:33 +0100

    More constexpr for constant rectangles.

--- unit_tests/test_rect.cpp
+++ unit_tests/test_rect.cpp
@@ -52,14 +52,14 @@ TEST(Rect, Null) {
 }
 
 TEST(Rect, InsetEmpty) {
-  draw::rect const r = draw::rect{}.inset(1, 1);
+  constexpr draw::rect r = draw::rect{}.inset(1, 1);
   EXPECT_EQ(r.width(), 0);
   EXPECT_EQ(r.height(), 0);
   EXPECT_TRUE(r.empty());
 }
 
 TEST(Rect, InsetSmaller) {
-  draw::rect const r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(1, 1);
+  constexpr draw::rect r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(1, 1);
   EXPECT_EQ(r, (draw::rect{.top = 11, .left = 11, .bottom = 19, .right = 19}));
   EXPECT_EQ(r.width(), 8);
   EXPECT_EQ(r.height(), 8);
@@ -67,7 +67,7 @@ TEST(Rect, InsetSmaller) {
 }
 
 TEST(Rect, InsetLarger1) {
-  draw::rect const r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(-1, -1);
+  constexpr draw::rect r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(-1, -1);
   EXPECT_EQ(r, (draw::rect{.top = 9, .left = 9, .bottom = 21, .right = 21}));
   EXPECT_EQ(r.width(), 12);
   EXPECT_EQ(r.height(), 12);
@@ -75,7 +75,7 @@ TEST(Rect, InsetLarger1) {
 }
 
 TEST(Rect, InsetLarger2) {
-  draw::rect const r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(-5, -5);
+  constexpr draw::rect r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(-5, -5);
   EXPECT_EQ(r, (draw::rect{.top = 5, .left = 5, .bottom = 25, .right = 25}));
   EXPECT_EQ(r.width(), 20);
   EXPECT_EQ(r.height(), 20);
@@ -83,7 +83,7 @@ TEST(Rect, InsetLarger2) {
 }
 
 TEST(Rect, InsetToEmpty) {
-  draw::rect const r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(1, 1);
+  constexpr draw::rect r = draw::rect{.top = 10, .left = 10, .bottom = 20, .right = 20}.inset(1, 1);
   EXPECT_EQ(r, (draw::rect{.top = 11, .left = 11, .bottom = 19, .right = 19}));
   EXPECT_EQ(r.width(), 8);
   EXPECT_EQ(r.height(), 8);
